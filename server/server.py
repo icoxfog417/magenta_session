e4ce0c1dbe4b2cc646ce901d55d24fbdf710e1a7
icoxfog417 <icoxfog417@yahoo.co.jp>
2017-03-29 21:51:50 +0900

    modify host

--- server/server.py
+++ server/server.py
@@ -35,4 +35,5 @@ def test():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    #app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(port=port, debug=True)
